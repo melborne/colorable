257fa9f456d11cb2a7c68e4620dd4823acb823d6
kyoendo <postagie@gmail.com>
2013-05-02 22:49:47 +0900

    Refactoring

--- spec/color_space_spec.rb
+++ spec/color_space_spec.rb
@@ -1,10 +1,10 @@
 require_relative 'spec_helper'
 
-describe Colorable::RGB do
-	let(:rgb) { Colorable::RGB }
+include Colorable
+describe RGB do
   describe ".new" do
   	context "without arguments" do
-  		subject	{ rgb.new }
+  		subject	{ RGB.new }
   		its(:to_a) { should eql [0, 0, 0] }
   		its(:rgb) { should eql [0, 0, 0] }
   		its(:red) { should eql 0 }
@@ -13,7 +13,7 @@ describe Colorable::RGB do
   	end
 
   	context "with arguments" do
-  		subject { rgb.new 100, 255, 255 }
+  		subject { RGB.new 100, 255, 255 }
   		its(:to_a) { should eql [100, 255, 255] }
   		its(:rgb) { should eql [100, 255, 255] }
   		its(:red) { should eql 100 }
@@ -23,16 +23,16 @@ describe Colorable::RGB do
 
   	context "pass out of RGB range" do
   		it "raise RangeError" do
-  			expect { rgb.new(0, 0, 256) }.to raise_error Colorable::ColorSpace::RangeError
-  			expect { rgb.new(-10, 0, 255) }.to raise_error Colorable::ColorSpace::RangeError
-  			expect { rgb.new(256, 256, 256) }.to raise_error Colorable::ColorSpace::RangeError
+  			expect { RGB.new(0, 0, 256) }.to raise_error ColorSpace::RangeError
+  			expect { RGB.new(-10, 0, 255) }.to raise_error ColorSpace::RangeError
+  			expect { RGB.new(256, 256, 256) }.to raise_error ColorSpace::RangeError
   		end
   	end
   end
 
   describe "#+" do
   	context "pass an array of numbers" do
-  		before(:all) { @rgb = rgb.new(100, 100, 100) }
+  		before(:all) { @rgb = RGB.new(100, 100, 100) }
   	  subject { @rgb + [0, 50, 100] }
   	  its(:rgb) { should eql [100, 150, 200] }
   	  it "keep original same" do @rgb.rgb.should eql [100, 100, 100] end
@@ -40,31 +40,31 @@ describe Colorable::RGB do
 
   	context "when '+' makes out of RGB range or rack of numbers" do
   		it "raise RangeError" do
-	  		expect { rgb.new(100, 100, 100) + [0, 50, 200] }.to raise_error Colorable::ColorSpace::RangeError
-	  		expect { rgb.new(100, 100, 100) + [0, -150, 0] }.to raise_error Colorable::ColorSpace::RangeError
+	  		expect { RGB.new(100, 100, 100) + [0, 50, 200] }.to raise_error ColorSpace::RangeError
+	  		expect { RGB.new(100, 100, 100) + [0, -150, 0] }.to raise_error ColorSpace::RangeError
       end
 
       it "raise ArgumentError" do
-	  		expect { rgb.new(100, 100, 100) + [0, 150] }.to raise_error ArgumentError
+	  		expect { RGB.new(100, 100, 100) + [0, 150] }.to raise_error ArgumentError
   		end
   	end
 
   	context "pass a Fixnum" do
-  		before(:all) { @rgb = rgb.new(100, 100, 100) }
+  		before(:all) { @rgb = RGB.new(100, 100, 100) }
   		it { (@rgb + 50).rgb.should eql [150, 150, 150] }
   		it "raise RangeError" do
-  		  expect { @rgb + 160 }.to raise_error Colorable::ColorSpace::RangeError
+  		  expect { @rgb + 160 }.to raise_error ColorSpace::RangeError
   		end
   	end
 
     context "coerce make Fixnum#+ accept rgb object" do
-      it { (10 + rgb.new(100, 100, 100)).rgb.should eql [110, 110, 110] }
+      it { (10 + RGB.new(100, 100, 100)).rgb.should eql [110, 110, 110] }
     end
   end
 
   describe "#-" do
   	context "pass an array of numbers" do
-  		before(:all) { @rgb = rgb.new(100, 100, 100) }
+  		before(:all) { @rgb = RGB.new(100, 100, 100) }
   	  subject { @rgb - [0, 50, 100] }
   	  its(:rgb) { should eql [100, 50, 0] }
   	  it "keep original same" do @rgb.rgb.should eql [100, 100, 100] end
@@ -72,30 +72,29 @@ describe Colorable::RGB do
 
   	context "when '-' makes out of RGB range" do
   		it "raise RangeError" do
-	  		expect { rgb.new(100, 100, 100) - [0, 50, 200] }.to raise_error Colorable::ColorSpace::RangeError
-	  		expect { rgb.new(100, 100, 100) - [0, -250, 0] }.to raise_error Colorable::ColorSpace::RangeError
+	  		expect { RGB.new(100, 100, 100) - [0, 50, 200] }.to raise_error ColorSpace::RangeError
+	  		expect { RGB.new(100, 100, 100) - [0, -250, 0] }.to raise_error ColorSpace::RangeError
   		end
   	end
 
   	context "pass a Fixnum" do
-  		before(:all) { @rgb = rgb.new(100, 100, 100) }
+  		before(:all) { @rgb = RGB.new(100, 100, 100) }
   		it { (@rgb - 50).rgb.should eql [50, 50, 50] }
   		it "raise RangeError" do
-  		  expect { @rgb - 160 }.to raise_error Colorable::ColorSpace::RangeError
+  		  expect { @rgb - 160 }.to raise_error ColorSpace::RangeError
   		end
   	end
   end
 
   describe "#to_s" do
-    it { rgb.new(0, 50, 100).to_s.should eql "rgb(0,50,100)"}
+    it { RGB.new(0, 50, 100).to_s.should eql "rgb(0,50,100)"}
   end
 end
 
-describe Colorable::HSB do
-	let(:hsb) { Colorable::HSB }
+describe HSB do
   describe ".new" do
   	context "without arguments" do
-  		subject	{ hsb.new }
+  		subject	{ HSB.new }
   		its(:to_a) { should eql [0, 0, 0] }
   		its(:hsb) { should eql [0, 0, 0] }
   		its(:hue) { should eql 0 }
@@ -104,7 +103,7 @@ describe Colorable::HSB do
   	end
 
   	context "with arguments" do
-  		subject { hsb.new 300, 70, 90 }
+  		subject { HSB.new 300, 70, 90 }
   		its(:to_a) { should eql [300, 70, 90] }
   		its(:hsb) { should eql [300, 70, 90] }
   		its(:hue) { should eql 300 }
@@ -114,19 +113,19 @@ describe Colorable::HSB do
 
   	context "pass out of HSB range" do
   		it "raise RangeError" do
-  			expect { hsb.new(0, 0, 120) }.to raise_error Colorable::ColorSpace::RangeError
-  			expect { hsb.new(360, 0, 80) }.to raise_error Colorable::ColorSpace::RangeError
+  			expect { HSB.new(0, 0, 120) }.to raise_error ColorSpace::RangeError
+  			expect { HSB.new(360, 0, 80) }.to raise_error ColorSpace::RangeError
   		end
 
       it "raise ArgumentError" do
-        expect { hsb.new(0, -10, 0) }.to raise_error Colorable::ColorSpace::RangeError
+        expect { HSB.new(0, -10, 0) }.to raise_error ColorSpace::RangeError
       end
   	end
   end
 
   describe "#+" do
   	context "pass an array of numbers" do
-  		before(:all) { @hsb = hsb.new(300, 70, 90) }
+  		before(:all) { @hsb = HSB.new(300, 70, 90) }
   	  subject { @hsb + [0, 5, 10] }
   	  its(:hsb) { should eql [300, 75, 100] }
   	  it "keep original same" do @hsb.hsb.should eql [300, 70, 90] end
@@ -134,20 +133,20 @@ describe Colorable::HSB do
 
   	context "when '+' makes out of HSB range or rack of numbers" do
   		it "raise RangeError" do
-	  		expect { hsb.new(300, 70, 90) + [0, 50, 0] }.to raise_error Colorable::ColorSpace::RangeError
-	  		expect { hsb.new(300, 70, 90) + [0, 0, -100] }.to raise_error Colorable::ColorSpace::RangeError
+	  		expect { HSB.new(300, 70, 90) + [0, 50, 0] }.to raise_error ColorSpace::RangeError
+	  		expect { HSB.new(300, 70, 90) + [0, 0, -100] }.to raise_error ColorSpace::RangeError
       end
 
       it "raise ArgumentError" do
-	  		expect { hsb.new(300, 70, 90) + [0, 5] }.to raise_error ArgumentError
-        expect { hsb.new(300, 70, 90) + 5 }.to raise_error ArgumentError
+	  		expect { HSB.new(300, 70, 90) + [0, 5] }.to raise_error ArgumentError
+        expect { HSB.new(300, 70, 90) + 5 }.to raise_error ArgumentError
   		end
   	end
   end
 
   describe "#-" do
     context "pass an array of numbers" do
-      before(:all) { @hsb = hsb.new(300, 70, 90) }
+      before(:all) { @hsb = HSB.new(300, 70, 90) }
       subject { @hsb - [0, 5, 10] }
       its(:hsb) { should eql [300, 65, 80] }
       it "keep original same" do @hsb.hsb.should eql [300, 70, 90] end
@@ -155,13 +154,13 @@ describe Colorable::HSB do
 
     context "when '-' makes out of HSB range" do
       it "raise RangeError" do
-        expect { hsb.new(300, 7, 90) - [305, 0, 10] }.to raise_error Colorable::ColorSpace::RangeError
-        expect { hsb.new(300, 70, 90) - [0, -35, 0] }.to raise_error Colorable::ColorSpace::RangeError
+        expect { HSB.new(300, 7, 90) - [305, 0, 10] }.to raise_error ColorSpace::RangeError
+        expect { HSB.new(300, 70, 90) - [0, -35, 0] }.to raise_error ColorSpace::RangeError
       end
     end
   end
 
   describe "#to_s" do
-    it { hsb.new(0, 50, 100).to_s.should eql "hsb(0,50,100)"}
+    it { HSB.new(0, 50, 100).to_s.should eql "hsb(0,50,100)"}
   end
 end
